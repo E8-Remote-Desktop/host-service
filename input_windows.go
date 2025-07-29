e2f6660e6a5776a55763d9586a94fe69dcae99f8
Rahul Chandra <rahul@projecteclipse.org>
2025-07-29 03:09:06 -0400

    hopefully this works?

--- input_windows.go
+++ input_windows.go
@@ -3,11 +3,36 @@
 
 package rdp
 
+/*
+#include <windows.h>
+
+// Helper function to send keyboard input
+void sendKeyboardInput(WORD vk, BOOL keyDown) {
+	INPUT input = {0};
+	input.type = INPUT_KEYBOARD;
+	input.ki.wVk = vk;
+	if (!keyDown) {
+		input.ki.dwFlags = KEYEVENTF_KEYUP;
+	}
+	SendInput(1, &input, sizeof(INPUT));
+}
+
+// Helper function to send mouse input
+void sendMouseInput(DWORD flags, LONG dx, LONG dy, DWORD mouseData) {
+	INPUT input = {0};
+	input.type = INPUT_MOUSE;
+	input.mi.dx = dx;
+	input.mi.dy = dy;
+	input.mi.dwFlags = flags;
+	input.mi.mouseData = mouseData;
+	SendInput(1, &input, sizeof(INPUT));
+}
+*/
+import "C"
+
 import (
 	"encoding/binary"
 	"log"
-	"syscall"
-	"unsafe"
 
 	"github.com/pion/webrtc/v3"
 )
@@ -16,90 +41,18 @@ func GetInput() Input {
 	return &RDPWindowsInput{}
 }
 
-var (
-	user32               = syscall.NewLazyDLL("user32.dll")
-	procSendInput        = user32.NewProc("SendInput")
-	procGetCursorPos     = user32.NewProc("GetCursorPos")
-	procSetCursorPos     = user32.NewProc("SetCursorPos")
-	procGetSystemMetrics = user32.NewProc("GetSystemMetrics")
-)
-
-const (
-	INPUT_MOUSE    = 0
-	INPUT_KEYBOARD = 1
-
-	// Mouse flags
-	MOUSEEVENTF_MOVE       = 0x0001
-	MOUSEEVENTF_LEFTDOWN   = 0x0002
-	MOUSEEVENTF_LEFTUP     = 0x0004
-	MOUSEEVENTF_RIGHTDOWN  = 0x0008
-	MOUSEEVENTF_RIGHTUP    = 0x0010
-	MOUSEEVENTF_MIDDLEDOWN = 0x0020
-	MOUSEEVENTF_MIDDLEUP   = 0x0040
-	MOUSEEVENTF_WHEEL      = 0x0800
-	MOUSEEVENTF_ABSOLUTE   = 0x8000
-
-	// Keyboard flags
-	KEYEVENTF_KEYUP = 0x0002
-
-	// Virtual key codes
-	VK_LCONTROL = 0xA2
-	VK_LSHIFT   = 0xA0
-	VK_LMENU    = 0xA4 // Left Alt
-	VK_LWIN     = 0x5B // Left Windows key
-
-	// System metrics
-	SM_CXSCREEN = 0
-	SM_CYSCREEN = 1
-
-	WHEEL_DELTA = 120
-)
-
-type POINT struct {
-	X, Y int32
-}
-
-type INPUT struct {
-	Type uint32
-	_    [4]byte // padding for alignment
-	Data [24]byte
-}
-
-type MOUSEINPUT struct {
-	Dx          int32
-	Dy          int32
-	MouseData   uint32
-	DwFlags     uint32
-	Time        uint32
-	DwExtraInfo uintptr
-}
-
-type KEYBDINPUT struct {
-	Vk          uint16
-	Scan        uint16
-	DwFlags     uint32
-	Time        uint32
-	DwExtraInfo uintptr
-}
-
 type RDPWindowsInput struct {
-	lastButtons  byte
-	screenWidth  int32
-	screenHeight int32
+	lastButtons byte
 }
 
 func (input *RDPWindowsInput) Init() error {
-	// Get screen dimensions for absolute positioning if needed
-	input.screenWidth = int32(getSystemMetrics(SM_CXSCREEN))
-	input.screenHeight = int32(getSystemMetrics(SM_CYSCREEN))
-
-	log.Println("Windows input handler initialized")
+	// Nothing special to initialize for Windows input injection
+	log.Println("Windows input initialized")
 	return nil
 }
 
 func (input *RDPWindowsInput) Close() {
-	// No cleanup needed for Windows API
-	log.Println("Windows input handler closed")
+	// Nothing to close on Windows
 }
 
 func (input *RDPWindowsInput) AcceptDataChannel(pc *webrtc.PeerConnection) {
@@ -110,7 +63,7 @@ func (input *RDPWindowsInput) processor(dc *webrtc.DataChannel) {
 	log.Printf("Data Channel %s request received\n", dc.Label())
 
 	dc.OnOpen(func() {
-		log.Printf("Data Channel %s opened, Input Ready\n", dc.Label())
+		log.Printf("Data Channel %s opened, Input HID Ready\n", dc.Label())
 	})
 
 	dc.OnMessage(func(msg webrtc.DataChannelMessage) {
@@ -129,8 +82,7 @@ func (input *RDPWindowsInput) processor(dc *webrtc.DataChannel) {
 			modifiers := data[3]
 
 			input.handleModifiers(modifiers, true)
-			input.sendKeyboardInput(uint16(keyCode), false) // Key down
-			input.sendKeyboardInput(uint16(keyCode), true)  // Key up
+			input.keyPressWindows(keyCode)
 			input.handleModifiers(modifiers, false)
 
 		case 2: // Mouse move + buttons
@@ -138,11 +90,12 @@ func (input *RDPWindowsInput) processor(dc *webrtc.DataChannel) {
 				log.Println("Invalid mouse move packet")
 				return
 			}
+
 			buttons := data[1]
 			dx := int16(binary.BigEndian.Uint16(data[2:4]))
 			dy := int16(binary.BigEndian.Uint16(data[4:6]))
 
-			input.sendMouseMove(int32(dx), int32(dy))
+			input.mouseMoveWindows(int32(dx), int32(dy))
 			input.handleMouseButtons(buttons)
 
 		case 3: // Scroll
@@ -150,11 +103,9 @@ func (input *RDPWindowsInput) processor(dc *webrtc.DataChannel) {
 				log.Println("Invalid scroll packet")
 				return
 			}
-			deltaY := int32(binary.BigEndian.Uint16(data[1:3]))
 
-			if deltaY != 0 {
-				input.sendMouseWheel(deltaY)
-			}
+			deltaY := int16(binary.BigEndian.Uint16(data[1:3]))
+			input.mouseScrollWindows(deltaY)
 
 		default:
 			log.Printf("Unknown input event type: %d", data[0])
@@ -162,51 +113,17 @@ func (input *RDPWindowsInput) processor(dc *webrtc.DataChannel) {
 	})
 }
 
-func (input *RDPWindowsInput) sendKeyboardInput(vkCode uint16, keyUp bool) {
-	var keyInput INPUT
-	keyInput.Type = INPUT_KEYBOARD
-
-	kbd := (*KEYBDINPUT)(unsafe.Pointer(&keyInput.Data[0]))
-	kbd.Vk = vkCode
-	kbd.Scan = 0
-	kbd.DwFlags = 0
-	if keyUp {
-		kbd.DwFlags = KEYEVENTF_KEYUP
+// KeyPress sends a key press (down + up) to Windows using Virtual-Key codes
+func (input *RDPWindowsInput) keyPressWindows(keyCode uint16) {
+	vk := windowsVirtualKey(keyCode)
+	if vk == 0 {
+		log.Printf("Unknown keycode %d", keyCode)
+		return
 	}
-	kbd.Time = 0
-	kbd.DwExtraInfo = 0
-
-	sendInput(1, &keyInput)
-}
-
-func (input *RDPWindowsInput) sendMouseMove(dx, dy int32) {
-	var mouseInput INPUT
-	mouseInput.Type = INPUT_MOUSE
-
-	mouse := (*MOUSEINPUT)(unsafe.Pointer(&mouseInput.Data[0]))
-	mouse.Dx = dx
-	mouse.Dy = dy
-	mouse.MouseData = 0
-	mouse.DwFlags = MOUSEEVENTF_MOVE
-	mouse.Time = 0
-	mouse.DwExtraInfo = 0
-
-	sendInput(1, &mouseInput)
-}
-
-func (input *RDPWindowsInput) sendMouseWheel(delta int32) {
-	var mouseInput INPUT
-	mouseInput.Type = INPUT_MOUSE
-
-	mouse := (*MOUSEINPUT)(unsafe.Pointer(&mouseInput.Data[0]))
-	mouse.Dx = 0
-	mouse.Dy = 0
-	mouse.MouseData = uint32(delta * WHEEL_DELTA)
-	mouse.DwFlags = MOUSEEVENTF_WHEEL
-	mouse.Time = 0
-	mouse.DwExtraInfo = 0
-
-	sendInput(1, &mouseInput)
+	// Key down
+	C.sendKeyboardInput(C.WORD(vk), C.TRUE)
+	// Key up
+	C.sendKeyboardInput(C.WORD(vk), C.FALSE)
 }
 
 func (input *RDPWindowsInput) handleModifiers(modifiers byte, press bool) {
@@ -214,17 +131,32 @@ func (input *RDPWindowsInput) handleModifiers(modifiers byte, press bool) {
 		mask byte
 		vk   uint16
 	}{
-		{1 << 0, VK_LCONTROL},
-		{1 << 1, VK_LSHIFT},
-		{1 << 2, VK_LMENU},
-		{1 << 3, VK_LWIN},
+		{1 << 0, 0xA2}, // Left Ctrl: VK_LCONTROL
+		{1 << 1, 0xA0}, // Left Shift: VK_LSHIFT
+		{1 << 2, 0xA4}, // Left Alt: VK_LMENU
+		{1 << 3, 0x5B}, // Left Meta (Windows key): VK_LWIN
 	}
 
 	for _, mod := range modKeys {
 		if modifiers&mod.mask != 0 {
-			input.sendKeyboardInput(mod.vk, !press)
+			if press {
+				C.sendKeyboardInput(C.WORD(mod.vk), C.TRUE)
+			} else {
+				C.sendKeyboardInput(C.WORD(mod.vk), C.FALSE)
+			}
 		}
 	}
+}
+
+func (input *RDPWindowsInput) mouseMoveWindows(dx, dy int32) {
+	// Send relative mouse movement
+	C.sendMouseInput(C.MOUSEEVENTF_MOVE, C.LONG(dx), C.LONG(dy), 0)
+}
+
+func (input *RDPWindowsInput) mouseScrollWindows(deltaY int16) {
+	// Vertical scroll wheel: WHEEL_DELTA = 120 per notch
+	const WHEEL_DELTA = 120
+	C.sendMouseInput(C.MOUSEEVENTF_WHEEL, 0, 0, C.DWORD(int32(deltaY)*WHEEL_DELTA))
 }
 
 func (input *RDPWindowsInput) handleMouseButtons(buttons byte) {
@@ -235,66 +167,46 @@ func (input *RDPWindowsInput) handleMouseButtons(buttons byte) {
 	}
 
 	buttonMap := []struct {
-		mask     byte
-		downFlag uint32
-		upFlag   uint32
+		mask    byte
+		press   uint32
+		release uint32
 	}{
-		{1 << 0, MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_LEFTUP},
-		{1 << 1, MOUSEEVENTF_RIGHTDOWN, MOUSEEVENTF_RIGHTUP},
-		{1 << 2, MOUSEEVENTF_MIDDLEDOWN, MOUSEEVENTF_MIDDLEUP},
+		{1 << 0, C.MOUSEEVENTF_LEFTDOWN, C.MOUSEEVENTF_LEFTUP},
+		{1 << 1, C.MOUSEEVENTF_RIGHTDOWN, C.MOUSEEVENTF_RIGHTUP},
+		{1 << 2, C.MOUSEEVENTF_MIDDLEDOWN, C.MOUSEEVENTF_MIDDLEUP},
 	}
 
 	for _, btn := range buttonMap {
 		if changed&btn.mask != 0 {
-			var flag uint32
 			if buttons&btn.mask != 0 {
-				flag = btn.downFlag
+				// Press
+				C.sendMouseInput(btn.press, 0, 0, 0)
 			} else {
-				flag = btn.upFlag
+				// Release
+				C.sendMouseInput(btn.release, 0, 0, 0)
 			}
-			input.sendMouseButtonInput(flag)
 		}
 	}
 
 	input.lastButtons = buttons
 }
 
-func (input *RDPWindowsInput) sendMouseButtonInput(flag uint32) {
-	var mouseInput INPUT
-	mouseInput.Type = INPUT_MOUSE
+// windowsVirtualKey converts your incoming keyCode to a Windows Virtual-Key code.
+// This is a simplified example; you will likely need a full mapping for your use case.
+func windowsVirtualKey(keyCode uint16) uint16 {
+	// Directly use the keyCode if it maps well or implement mapping
+	// For example: 0x41 = 'A' in ASCII and VK_A = 0x41
+	// You should expand this mapping as per your protocol
 
-	mouse := (*MOUSEINPUT)(unsafe.Pointer(&mouseInput.Data[0]))
-	mouse.Dx = 0
-	mouse.Dy = 0
-	mouse.MouseData = 0
-	mouse.DwFlags = flag
-	mouse.Time = 0
-	mouse.DwExtraInfo = 0
+	// For demo: assume keyCode is ASCII and map A-Z, 0-9, arrows, etc.
+	if keyCode >= 0x41 && keyCode <= 0x5A { // A-Z
+		return keyCode
+	}
+	if keyCode >= 0x30 && keyCode <= 0x39 { // 0-9
+		return keyCode
+	}
 
-	sendInput(1, &mouseInput)
-}
+	// Add special keys mapping as needed here
 
-// Helper functions for Windows API calls
-func sendInput(nInputs uint32, pInputs *INPUT) uint32 {
-	ret, _, _ := procSendInput.Call(
-		uintptr(nInputs),
-		uintptr(unsafe.Pointer(pInputs)),
-		unsafe.Sizeof(INPUT{}),
-	)
-	return uint32(ret)
-}
-
-func getSystemMetrics(nIndex int32) int32 {
-	ret, _, _ := procGetSystemMetrics.Call(uintptr(nIndex))
-	return int32(ret)
-}
-
-func getCursorPos() (x, y int32) {
-	var pt POINT
-	procGetCursorPos.Call(uintptr(unsafe.Pointer(&pt)))
-	return pt.X, pt.Y
-}
-
-func setCursorPos(x, y int32) {
-	procSetCursorPos.Call(uintptr(x), uintptr(y))
+	return 0 // unknown key
 }
