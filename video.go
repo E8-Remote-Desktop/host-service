b4a11bc7c44c4c297e8d99f1649658f38bea7216
Rahul Chandra <rahul@projecteclipse.org>
2025-08-23 23:30:29 -0400

    fix parsing error

--- video.go
+++ video.go
@@ -55,7 +55,7 @@ func (video *RDPAudioVideo) buildGstVideoPipeline(config *StreamConfig) string {
 		// usage=2 because in newer AMD drivers usage=1 for ultra low latency doesn't seem to function
 		pipeline = fmt.Sprintf("d3d11screencapturesrc monitor-index=%d show-cursor=true ", config.screen) +
 			"! d3d11convert" +
-			fmt.Sprintf("! 'video/x-raw(memory:D3D11Memory),framerate=%s/1,format=NV12' ", config.framerate) +
+			fmt.Sprintf("! video/x-raw(memory:D3D11Memory),framerate=%s/1,format=NV12 ", config.framerate) +
 			fmt.Sprintf("! amf%senc rate-control=cbr bitrate=%s gop-size=%s usage=2", config.codec, config.bitrate, config.gopsize) +
 			"! queue max-size-buffers=1 max-size-time=0 max-size-bytes=0 leaky=downstream " +
 			fmt.Sprintf("! video/x-%s,stream-format=byte-stream,alignment=au ", config.codec) +
