b7d7efcbe538d18ebc8d462f1a423fcdf0d9872e
Rahul Chandra <rahul@projecteclipse.org>
2025-08-23 23:39:04 -0400

    more spaces

--- video.go
+++ video.go
@@ -56,7 +56,7 @@ func (video *RDPAudioVideo) buildGstVideoPipeline(config *StreamConfig) string {
 	case "amd":
 		// usage=2 because in newer AMD drivers usage=1 for ultra low latency doesn't seem to function
 		pipeline = fmt.Sprintf("d3d11screencapturesrc monitor-index=%d show-cursor=true ", config.screen) +
-			"! d3d11convert" +
+			"! d3d11convert " +
 			fmt.Sprintf("! 'video/x-raw(memory:D3D11Memory),framerate=%d/1,format=NV12' ", config.framerate) +
 			fmt.Sprintf("! amf%senc rate-control=cbr bitrate=%d gop-size=%d usage=2 ", config.codec, config.bitrate, config.gopsize) +
 			"! queue max-size-buffers=1 max-size-time=0 max-size-bytes=0 leaky=downstream " +
