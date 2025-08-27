539ddff7da6bab1adfcda4861170cc64c7b8b41f
Rahul Chandra <rahul@projecteclipse.org>
2025-08-27 18:05:41 -0400

    more?

--- video.go
+++ video.go
@@ -301,7 +301,7 @@ func (video *RDPAudioVideo) receiveRTPAndForward(ctx context.Context, listenAddr
 	// optimize for around 1ms (1000 microSecond) latency
 	//minPacketInterval := time.Duration(1000/((config.bitrate)/((config.mtu*8)/1000))) * time.Microsecond
 	// set packet smoothing 1 microsecond for every 5 mbit
-	minPacketInterval := time.Duration(10*(config.bitrate/5000)) * time.Microsecond
+	minPacketInterval := time.Duration(100*(config.bitrate/5000)) * time.Microsecond
 	log.Printf("Selected pacing of %v\n", minPacketInterval)
 
 	for {
