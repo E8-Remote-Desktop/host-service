9cde043d257cdff3a0928dee27346e183382fe64
Rahul Chandra <rahul@projecteclipse.org>
2025-08-27 17:00:53 -0400

    remove log

--- video.go
+++ video.go
@@ -343,7 +343,6 @@ func (video *RDPAudioVideo) receiveRTPAndForward(ctx context.Context, listenAddr
 			now := time.Now()
 			if !lastSendTime.IsZero() {
 				elapsed := now.Sub(lastSendTime)
-				log.Printf("%s time since last packet", elapsed)
 				if elapsed < minPacketInterval {
 					time.Sleep(minPacketInterval - elapsed)
 				}
