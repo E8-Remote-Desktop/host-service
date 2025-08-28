c4e8682e65b432233365f2f16f8b551ff400856d
Rahul Chandra <rahul@projecteclipse.org>
2025-08-28 00:48:28 -0400

    match buffer

--- video.go
+++ video.go
@@ -66,7 +66,6 @@ func (video *RDPAudioVideo) buildGstVideoPipeline(config *StreamConfig) string {
 			"! queue max-size-buffers=1 max-size-time=0 max-size-bytes=0 leaky=downstream " +
 			fmt.Sprintf("! video/x-%s,stream-format=byte-stream,alignment=au ", config.codec) +
 			fmt.Sprintf("! rtp%spay config-interval=0 pt=96 aggregate-mode=zero-latency mtu=%d ", rtpCodec, config.mtu) +
-			"! queue max-size-time=20000000 leaky=downstream " +
 			"! udpsink host=127.0.0.1 port=50055 sync=false async=false "
 		log.Println(pipeline)
 	}
@@ -284,7 +283,7 @@ func (video *RDPAudioVideo) receiveRTPAndForward(ctx context.Context, listenAddr
 		   latency jumps by 200ms+, and it's impossible to tell that it's this
 		   var
 		*/
-		udpConn.SetReadBuffer(1024 * 1024 * 1.5)
+		udpConn.SetReadBuffer(1024 * 208)
 	}
 
 	// Goroutine to handle context cancellation
